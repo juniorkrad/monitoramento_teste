2fb54a6d9b94e98a694d653cc5686166d42ffddf
Junior <juniorkrad@gmail.com>
2026-03-05 12:19:23 -0300

    menu - icone de energia

--- layout.js
+++ layout.js
@@ -1,5 +1,5 @@
 // ==============================================================================
-// layout.js - Construtor de Layout e Menu Inteligente (Versão 2.3 - Módulo Energia)
+// layout.js - Construtor de Layout e Menu Inteligente (Versão 2.4 - Ajuste Visual Energia)
 // ==============================================================================
 
 // --- LISTA MESTRA DE PÁGINAS ---
@@ -137,7 +137,7 @@ function loadSidebar(currentPage) {
                 </a>
 
                 <a href="energia.html" class="sidebar-link home-highlight" style="margin-top: 5px; background-color: rgba(0,0,0,0.2); font-size: 1rem; padding: 12px;">
-                    <span class="material-symbols-rounded" style="font-size: 24px; color: #f87171;">battery_alert</span>
+                    <span class="material-symbols-rounded" style="font-size: 24px;">bolt</span>
                     ENERGIA
                 </a>
                 
@@ -165,7 +165,7 @@ function toggleSidebar() {
 }
 
 /**
- * Rodapé (ATUALIZADO)
+ * Rodapé
  */
 function loadFooter() {
     const footerPlaceholder = document.getElementById('footer-placeholder');
